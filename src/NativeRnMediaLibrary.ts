6b5814a7c10d5c82f8e7bac3acd24006399009c5
navvitech <navaneethreddyboda@gmail.com>
2024-12-29 02:56:37 +0530

    fix: 🐛 fixed errors in spec file

--- src/NativeRnMediaLibrary.ts
+++ src/NativeRnMediaLibrary.ts
@@ -2,18 +2,18 @@ import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
-  getAssets(assetConfig: Object): Promise<String>;
+  getAssets(assetConfig: Object): Promise<string>;
   getConstants: () => {
     MEDIA_TYPE: Object;
   };
-  getAudioFileInfo(audioId: String): Promise<string>;
+  getAudioFileInfo(audioId: string): Promise<string>;
   getAlbums(albumConfig: Object): Promise<string>;
-  getAlbumAssets(assetConfig: Object, id: String): Promise<string>;
-  getArtists: Promise<string>;
-  getArtistAudio(artistId: String): Promise<string>;
-  getGenres: Promise<string>;
-  getGenreAudio(genreId: String, assetConfig: Object): Promise<string>;
-  getFoldersAndFiles(path: String): Promise<string>;
+  getAlbumAssets(assetConfig: Object, id: string): Promise<string>;
+  getArtists(): Promise<string>;
+  getArtistAudio(artistId: string): Promise<string>;
+  getGenres(): Promise<string>;
+  getGenreAudio(genreId: string, assetConfig: Object): Promise<string>;
+  getFoldersAndFiles(path: string): Promise<string>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('RnMediaLibrary');
